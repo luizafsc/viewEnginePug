3764d0fc57c68bcc9e94c9d1a4f35aa76401338a
luiza fsc <luizascrs@gmail.com>
2023-10-17 23:19:21 -0300

    quase pronto

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Importe a biblioteca mysql2
+const mysql = require('mysql');
+
+// Configurações de conexão com o banco de dados
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: '',
+  password: '',
+  database: 'numeros'
+});
+
+// Realize a consulta SELECT
+const listaBuscada = 'lista1'; // Substitua pela lista que deseja buscar
+const query = `SELECT numero FROM listas_numeros WHERE lista_nome = ?`;
+
+connection.query(query, [listaBuscada], (error, results) => {
+  if (error) {
+    console.error('Ocorreu um erro ao buscar os dados:', error);
+  } else {
+    const numeros = results.map(row => row.numero);
+    console.log(`Os números da lista '${listaBuscada}' são:`, numeros);
+  }
+});
+
+// Feche a conexão com o banco de dados após a consulta
+connection.end();
+
+
 // Configurando o mecanismo de visualização Pug
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -10,7 +38,8 @@ app.set('views', path.join(__dirname, 'views'));
 const data = {
   pageTitle: 'Exemplo Pug Dinâmico com Express e Bootstrap',
   pageDescription: 'Esta é uma página de exemplo usando Pug com Express para valores dinâmicos e Bootstrap para layout responsivo.',
-  items: ['Item 1', 'Item 2', 'Item 3']
+  items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'],
+  obs: 'essa é uma observação'
 };
 
 // Rota para renderizar o template com os dados
